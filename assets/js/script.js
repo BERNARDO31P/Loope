c96c991570fb7e161d6987395286f4fa35c6e841
Bernardo de Oliveira <bernardo@bernardo.fm>
2020-05-11 12:38:03 +0200

    - Implemented PHPs array_chunk() in Javascript - Limited output of pictures

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,18 @@ let body = $("body"),
     suggestions = $("#suggestions"),
     buttons = $("#buttons");
 
+// Implementierung einer Funktionseigenschaft
+Object.defineProperty(Array.prototype, 'chunk_inefficient', {
+    value: function (chunkSize) {
+        let array = this;
+        return [].concat.apply([],
+            array.map(function (elem, i) {
+                return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
+            })
+        );
+    }
+});
+
 // Diese Funktion ersetzt die URL mit den nötigen GET Parameter
 function insertParams(keys, values) {
     let params = "?" + keys[0] + "=" + values[0];
@@ -151,7 +163,7 @@ function sendSearch(page = 1) {
                         if (page === 1) {
                             let object = '<div id="imageview">';
 
-                            $.each(data['info']['pictures'], function (key, info) {
+                            $.each(data['info']['pictures'].chunk_inefficient(20)[0], function (key, info) {
                                 let url = "./assets/php/download.php?dir=" + info['dirname'] + "&file=" + info['basename'];
                                 object += "<a href='" + url + "'><img class='images' src='" + url + "' alt='" + info['filename'] + "'/></a>";
                             })
@@ -165,7 +177,6 @@ function sendSearch(page = 1) {
 
                             let object = "<div class='page'><span>L</span>";
 
-                            // TODO: Falls mehr als 6 Seiten, wie Google dynamisch anzeigen
                             for (let i = 0; i < data['pages']; i++) {
                                 object += "<div class='pagenum'>";
                                 if (i + 1 === page) {
